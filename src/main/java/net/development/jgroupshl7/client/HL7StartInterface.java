c25b087aa5461d4a64983e98d4e00baaefdf630a
Yemi <yemie@Yemis-MacBook-Pro.local>
2024-05-19 16:17:02 +1000

    refactored for microservices

--- src/main/java/net/development/jgroupshl7/client/HL7StartInterface.java
+++ src/main/java/net/development/jgroupshl7/client/HL7StartInterface.java
@@ -3,8 +3,10 @@ package net.development.jgroupshl7.client;
 public class HL7StartInterface {
 	
 	public static void main(String[] args) throws Exception {
-    	System.setProperty("jgroups.bind_addr", "192.168.0.17");
-    	System.setProperty("jgroups.tcpping.initial_hosts", "192.168.0.17[7800],192.168.0.17[7801]");
+		String podIP = System.getenv("MY_POD_IP");
+		
+    	System.setProperty("jgroups.bind_addr", podIP);
+    	System.setProperty("jgroups.tcpping.initial_hosts", podIP + "[7800]," + podIP + "[7801]");
         
     	HL7MessageClient cluster = new HL7MessageClient();
     	cluster.start();
